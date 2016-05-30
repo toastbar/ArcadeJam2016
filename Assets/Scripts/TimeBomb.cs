86af2572d781e082ccf2e81cc97dd874ea99e63e
Jon Conrad <toastbar@gmail.com>
2016-05-29 20:53:28 -0600

    added bomb sprite

---
+++ Assets/Scripts/TimeBomb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TimeBomb : MonoBehaviour {
+	public float timer;
+	float createTime = 0;
+	// Use this for initialization
+	void Start () {
+		createTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//gameObject.GetComponent<SpriteRenderer>().sprite=Bomb_3;
+		if ((Time.time - createTime) > timer) {
+			Destroy (gameObject);
+		}
+
+	}
+}

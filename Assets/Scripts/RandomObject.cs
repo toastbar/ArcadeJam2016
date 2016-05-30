c32cba2e7ebc30cb6c68f22acfa5030965ea3d0c
Jon Conrad <toastbar@gmail.com>
2016-05-29 19:51:16 -0600

    rnd obj generator

---
+++ Assets/Scripts/RandomObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class RandomObject : MonoBehaviour {
+	public GameObject[] rndObjects;
+	// Use this for initialization
+	void Start () 
+	{
+		int rnd = Random.Range (0, rndObjects.Length - 1);
+		Object.Instantiate(rndObjects[rnd], transform.position, transform.rotation);
+		Object.Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
